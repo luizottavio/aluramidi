5be318a480770a3c316a45c2f8b06f6a0edddfd7
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-09-06 11:07:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,18 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contator = 0 
 
 while (contator < listaDeTeclas.length) { 
-    listaDeTeclas[contator].onclick = tocaSom;
+
+    const tecla = listaDeTeclas[contator];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    console.log(instrumento);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+
+    }
 
     contator ++
 
